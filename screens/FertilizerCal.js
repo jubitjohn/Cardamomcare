37e4ca9abd7f9d463dae98a2052076e244651651
anjusuresh1 <anjusuresh2001@gmail.com>
2023-06-06 19:46:13 +0530

    fert-calc

--- screens/FertilizerCal.js
+++ screens/FertilizerCal.js
@@ -16,17 +16,33 @@ const FertilizerCal= () => {
   const [plotSize, setPlotSize] = useState(0);
   const [result, setResult] = useState('');
   const [formCompleted, setFormCompleted] = useState(false);
+  const [showResult, setShowResult] = useState(false);
 
   const handleNChange = (value) => {
-    setNValue(value);
+    // setNValue(value);
+    if (value !== '') {
+      setNValue(parseInt(value));
+    } else {
+      setNValue(0);
+    }
   };
 
   const handlePChange = (value) => {
-    setPValue(value);
+    // setPValue(value);
+    if (value !== '') {
+      setPValue(parseInt(value));
+    } else {
+      setPValue(0);
+    }
   };
 
   const handleKChange = (value) => {
-    setKValue(value);
+    // setKValue(value);
+    if (value !== '') {
+      setKValue(parseInt(value));
+    } else {
+      setKValue(0);
+    }
   };
 
   const handleUnitChange = (value) => {
@@ -34,7 +50,12 @@ const FertilizerCal= () => {
   };
 
   const handlePlotSizeChange = (value) => {
-    setPlotSize(value);
+    // setPlotSize(value);
+    if (value !== '') {
+      setPlotSize(parseInt(value));
+    } else {
+      setPlotSize(0);
+    }
   };
 
   useEffect(() => {
@@ -48,6 +69,7 @@ const FertilizerCal= () => {
   const handleCalculate = () => {
     const resultText = `N:${nValue}, P:${pValue}, K:${kValue}, Unit:${unit}, Plot Size:${plotSize}`;
     setResult(resultText);
+    setShowResult(true);
   };
 
   // Add a useEffect hook to check form completion whenever the input values change
@@ -182,76 +204,41 @@ const FertilizerCal= () => {
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.section}>
+      <View style={styles.topsection}>
+      <View style={styles.nutsection}>
         <Text style={styles.sectionTitle}>Nutrient Quantities</Text>
         <View style={styles.mainsubbox}>
         <View style={styles.valueSelector}>
           <Text style={styles.valueLabel}>N</Text>
-          <View style={styles.nutrientval}>
-          <TouchableOpacity
-            style={styles.valueButtonnpk}
-            onPress={() => handleNChange(nValue - 1)}
-          >
-            <Text style={styles.buttonText}>-</Text>
-          </TouchableOpacity>
+          <View style={styles.inputout}>
           <TextInput
-            style={styles.valueInput}
+            style={styles.valueInputnpk}
             value={nValue.toString()}
             onChangeText={handleNChange}
             keyboardType="numeric"
           />
-          <TouchableOpacity
-            style={styles.valueButtonnpk}
-            onPress={() => handleNChange(nValue + 1)}
-          >
-            <Text style={styles.buttonText}>+</Text>
-          </TouchableOpacity>
           </View>
         </View>
         <View style={styles.valueSelector}>
           <Text style={styles.valueLabel}>P</Text>
-          <View style={styles.nutrientval}>
-          <TouchableOpacity
-            style={styles.valueButtonnpk}
-            onPress={() => handlePChange(pValue - 1)}
-          >
-            <Text style={styles.buttonText}>-</Text>
-          </TouchableOpacity>
+          <View style={styles.inputout}>
           <TextInput
-            style={styles.valueInput}
+            style={styles.valueInputnpk}
             value={pValue.toString()}
             onChangeText={handlePChange}
             keyboardType="numeric"
           />
-          <TouchableOpacity
-            style={styles.valueButtonnpk}
-            onPress={() => handlePChange(pValue + 1)}
-          >
-            <Text style={styles.buttonText}>+</Text>
-          </TouchableOpacity>
           </View>
         </View>
         <View style={styles.valueSelector}>
           <Text style={styles.valueLabel}>K</Text>
-          <View style={styles.nutrientval}>
-          <TouchableOpacity
-            style={styles.valueButtonnpk}
-            onPress={() => handleKChange(kValue - 1)}
-          >
-            <Text style={styles.buttonText}>-</Text>
-          </TouchableOpacity>
+          <View style={styles.inputout}>
           <TextInput
-            style={styles.valueInput}
+            style={styles.valueInputnpk}
             value={kValue.toString()}
             onChangeText={handleKChange}
             keyboardType="numeric"
           />
-          <TouchableOpacity
-            style={styles.valueButtonnpk}
-            onPress={() => handleKChange(kValue + 1)}
-          >
-            <Text style={styles.buttonText}>+</Text>
-          </TouchableOpacity>
           </View>
         </View>
         </View>
@@ -302,15 +289,6 @@ const FertilizerCal= () => {
           </TouchableOpacity>
         </View>
       </View>
-      {/* <TouchableOpacity
-        style={styles.calculateButton}
-        onPress={handleCalculate}
-      >
-        <Text style={styles.buttonText}>Calculate</Text>
-      </TouchableOpacity>
-      <View>
-        <Text>{result}</Text>
-      </View> */}
       <TouchableOpacity
         onPress={handleCalculate}
         disabled={!formCompleted}
@@ -318,7 +296,34 @@ const FertilizerCal= () => {
       >
         <Text style={styles.buttonText}>Calculate</Text>
       </TouchableOpacity>
-      
+      </View>
+      {showResult && (
+        <View style={styles.resultsection}>
+          <Text style={styles.cardAboveInfo}>Choose your preferred fertilizer combination:</Text>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <Text style={styles.cardHeading}>DAP/MOP/Urea</Text>
+            <View style={styles.cardInfoRow}>
+              <View style={styles.cardInfoColumn}>
+                <Text style={styles.cardInfoLabel}>DAP</Text>
+                <Text style={styles.cardInfoValue}>2296 kg</Text>
+                <Text style={styles.cardsubInfoValue}>46 Bags</Text>
+              </View>
+              <View style={styles.cardInfoColumn}>
+                <Text style={styles.cardInfoLabel}>MOP</Text>
+                <Text style={styles.cardInfoValue}>1447 KG</Text>
+                <Text style={styles.cardsubInfoValue}>29 bags</Text>
+              </View>
+              <View style={styles.cardInfoColumn}>
+                <Text style={styles.cardInfoLabel}>Urea</Text>
+                <Text style={styles.cardInfoValue}>224 kg</Text>
+                <Text style={styles.cardsubInfoValue}>8 Bags</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        </View>
+      )}
       
     </ScrollView>
     
@@ -330,11 +335,31 @@ const styles = StyleSheet.create({
     opacity:0.5
   },
   container: {
-    flex: 1,
-    padding: 25,
+    backgroundColor:'#E8E8E8',
   },
+  topsection:{
+    backgroundColor:'white',
+    height:570,
+    width: '100%',
+    padding:25,
+    borderBottomRightRadius:25,
+    borderBottomLeftRadius:25,
+    shadowColor: 'rgba(0, 0, 0, 0.3)',
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  // resultsection:{
+  //   backgroundColor:"pink",
+    
+  // },
   section: {
     marginBottom: 20,
+  },
+  nutsection: {
+    marginBottom: 20,
+    height:160
   },
   sectionunit:{
     marginBottom: 20,
@@ -346,31 +371,31 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingBottom: 10,
   },
-  sectionTitleplot:{
-
-  },
   valueSelector: {
-    flexDirection: "column",
+    flex:1,
     alignItems: "center",
     marginBottom: 0,
-    paddingRight:10, 
+    paddingRight:0, 
   },
-  valueSelectornpk: {
-    flexDirection: "column",
-    alignItems: "center",
-    height:40,
-    marginBottom: 0,
-    paddingRight:15, 
-    backgroundColor:"purple",
-  },
+  
+  // valueSelectornpk: {
+  //   flexDirection: "column",
+  //   alignItems: "center",
+  //   height:40,
+  //   marginBottom: 0,
+  //   paddingRight:5, 
+  //   backgroundColor:"purple",
+  // },
   nutrientval:{
-    flexDirection: "row",
-    alignItems: "center",
     borderRadius:20,
-    backgroundColor:"#E8E8E8",
+    // backgroundColor:"#E8E8E8",
+    backgroundColor:"pink",
     borderWidth:2,
     borderColor:"white",
     marginTop:6,
+    marginRight:10,
+    width:80,
+    alignItems: "center",
   },
   valueSelectorplot: {
     flexDirection: "row",
@@ -385,7 +410,6 @@ const styles = StyleSheet.create({
   },
   mainsubbox:{
     flex: 1,
-    height: 110,
     borderWidth: 3,
     borderColor: "white",
     backgroundColor: "#F8F8F8",
@@ -394,20 +418,103 @@ const styles = StyleSheet.create({
     padding:15,
     flexDirection: "row",
   },
+  valueInput:{
+    textAlign:"center",
+    fontSize:25,
+    fontWeight:"semibold",
+    width:80,
+    flexGrow:1
+  },
+  inputout:{
+    borderRadius:25,
+    // backgroundColor:"#E8E8E8",
+    backgroundColor:"#E8E8E8",
+    borderWidth:2,
+    borderColor:"white",
+    marginTop:6,
+    marginRight:10,
+    width:90,
+  },
+  valueInputnpk:{
+    borderColor: "white",
+    borderRadius:29,
+    backgroundColor: "#E8E8E8",
+    borderRadius: 10,
+    marginRight:10,
+    marginLeft:10,
+    padding:10,
+    textAlign:"center",
+    fontSize:16,
+    fontWeight:"semibold",
+  },
   valueInputplot:{
     flex: 1,
     height: 70,
     borderWidth: 1,
     borderColor: "white",
-    backgroundColor: "white",
+    backgroundColor: "#E8E8E8",
     borderRadius: 10,
     paddingHorizontal: 10,
     marginRight:10,
     marginLeft:10,
     textAlign:"center",
     fontSize:25,
-    fontWeight:"semibold"
-
+    fontWeight:"semibold",
+  },
+  cardContainer: {
+    // Styles for the card container
+    width: 320,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    marginTop: 20,
+    padding: 10,
+    borderRadius:20,
+    paddingBottom:40,
+    marginBottom:50,
+    shadowColor: 'rgba(0, 0, 0, 0.3)',
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  card: {
+    // Styles for the card
+  },
+  cardAboveInfo:{
+    paddingTop:20,
+    paddingLeft:18,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  cardHeading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+    padding: 10,
+    paddingLeft:5,
+    color:'#151515'
+  },
+  cardInfoRow: {
+    // Styles for the row containing the card information
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft:5,
+  },
+  cardInfoColumn: {
+    // Styles for each column in the row
+    flex: 1,
+  },
+  cardInfoLabel:{
+    color:'#474747',
+    fontSize: 15,
+    fontWeight: 500,
+  },
+  cardInfoValue:{
+    fontSize: 20,
+    color:"grey"
+  },
+  cardsubInfoValue:{
+    color:"grey"
   },
   valueButton: {
     width: 40,

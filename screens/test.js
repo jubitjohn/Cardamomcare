20649ff8cd0611842e4c9c5955866657e897d67e
jubitjohn <jubitjohn1@gmail.com>
2023-06-30 01:19:21 +0530

    popup button added

--- screens/test.js
+++ screens/test.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { PinchGestureHandler } from "react-native-gesture-handler";
 const windowHeight = Dimensions.get("window").height;
+import { FontAwesome } from "@expo/vector-icons";
 // const windowWidth = Dimensions.get("window").width;
 
 import {
@@ -32,6 +33,7 @@ export default function ImagePickerExample({ navigation }) {
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [photo, setPhoto] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -154,10 +156,6 @@ export default function ImagePickerExample({ navigation }) {
       resultOverlay = await dataOverlay.predictions[0].tagName;
       console.log("Is cardamom or not :", dataOverlay.predictions[0].tagName);
 
-      if (resultOverlay == "Cardamom") {
-        setIsCardamom(true);
-      }
-
       // Main model
 
       if (resultOverlay == "Cardamom") {
@@ -186,7 +184,6 @@ export default function ImagePickerExample({ navigation }) {
         // Code to be executed after the delay
 
         console.log("Delayed execution of prediction results");
-        navigation.navigate("ResultScreen", { res: result });
       }, 2000); // Delay of 2000 milliseconds (2 seconds)
     } catch (error) {
       console.error(error);
@@ -197,6 +194,11 @@ export default function ImagePickerExample({ navigation }) {
 
         // Call your function or perform the desired actions here
         setIsLoading(false);
+        if (resultOverlay !== "Cardamom") {
+          setIsDrawerOpen(true);
+        } else {
+          navigation.navigate("ResultScreen", { res: result });
+        }
       }, 2000);
       // Hide the dark overlay after API response
     }
@@ -206,17 +208,51 @@ export default function ImagePickerExample({ navigation }) {
     <View style={styles.container}>
       {photo && (
         <View style={styles.preview}>
-          <Image source={photo} style={styles.previewImage} />
-          <Button title="Retake" onPress={() => setPhoto(null)} />
-          <Button title="Diagnose" onPress={() => handleTakePhoto()} />
+          <Image
+            source={photo}
+            style={styles.previewImage}
+            resizeMode="cover"
+          />
+          <View style={styles.previewButtonContainer}>
+            <TouchableOpacity
+              style={styles.captureSubmitButton}
+              onPress={() => handleTakePhoto()}
+            >
+              <FontAwesome name="check" size={50} color="white" />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={styles.captureRetakeButton}
+            onPress={() => (setPhoto(null), setIsDrawerOpen(false))}
+          >
+            <FontAwesome5 name="chevron-left" size={34} color="#ffffff" />
+          </TouchableOpacity>
           {isLoading && (
             <View style={styles.overlay}>
               <View style={styles.overlayContent}>
                 <Animatable.View ref={animateIconRef}>
                   <FontAwesome5 name="leaf" size={iconSize} color={iconColor} />
                 </Animatable.View>
-
                 <Text style={styles.loadingText}>{loadingText}</Text>
+              </View>
+            </View>
+          )}
+          {isDrawerOpen && (
+            <View style={styles.drawerContainer}>
+              <View style={styles.drawerContent}>
+                <Text style={styles.drawerMessage}>
+                  Cardamom plant not identified
+                </Text>
+                <Text style={styles.drawerInstruction}>
+                  Please ensure that the image shows a close and clear view of
+                  the diseased part of the plant.
+                </Text>
+                <TouchableOpacity
+                  style={styles.scanAgainButton}
+                  onPress={() => (setPhoto(null), setIsDrawerOpen(false))}
+                >
+                  <Text style={styles.scanAgainButtonText}>Scan Again</Text>
+                </TouchableOpacity>
               </View>
             </View>
           )}
@@ -226,7 +262,6 @@ export default function ImagePickerExample({ navigation }) {
       {!photo && (
         <View style={{ flex: 1, maxHeight: windowHeight }}>
           <Camera
-            ratio="1:1"
             style={styles.camera}
             type={type}
             ref={(ref) => (cameraRef = ref)}
@@ -336,8 +371,14 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     backgroundColor: "transparent",
-    margin: 5,
-    paddingBottom: 30,
+    paddingBottom: 45,
+  },
+  previewButtonContainer: {
+    position: "absolute",
+    flex: 1,
+    backgroundColor: "transparent",
+    paddingBottom: 0,
+    bottom: 55,
   },
   cameraFrameContainer: {
     marginTop: 30,
@@ -375,6 +416,68 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
     borderColor: "#ccc",
     borderWidth: 5,
+  },
+  captureSubmitButton: {
+    flex: 1,
+    width: 85,
+    height: 85,
+    borderRadius: 999,
+    backgroundColor: "#046AE1",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  captureRetakeButton: {
+    flex: 1,
+    position: "absolute",
+    left: 55,
+    bottom: 75,
+    width: 50,
+    height: 50,
+    borderRadius: 999,
+    backgroundColor: "#046AE1",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  drawerContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    borderRadius: 30,
+    height: "35%",
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  drawerContent: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  drawerMessage: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000000",
+    textAlign: "center",
+  },
+  drawerInstruction: {
+    fontSize: 16,
+    color: "#000000",
+    textAlign: "center",
+    marginBottom: 10,
+    padding: 20,
+  },
+  scanAgainButton: {
+    backgroundColor: "#14ad38",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  scanAgainButtonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 
   infoButton: {
@@ -436,9 +539,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   previewImage: {
-    width: "90%",
-    height: "80%",
-    resizeMode: "contain",
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
   dot: {
     position: "absolute",
